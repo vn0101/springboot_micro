4278ef92ade4b3b5c7a4ed3b4f38704289528726
vn0101 <vindya.vn01@gmail.com>
2024-03-22 18:39:58 +0530

    initial commit

---
+++ mssc-beer-brew/src/main/java/guru/springframework/msscbeerbrew/MsscBeerBrewApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerbrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerBrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerBrewApplication.class, args);
+    }
+
+}
---
+++ mssc-beer-brew/src/test/java/guru/springframework/msscbeerbrew/MsscBeerBrewApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerbrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerBrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
